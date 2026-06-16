439a0b540dd82abc55cfca2b451bffe38461c7c5
Rebekah Esmaili <rebekah.esmaili@gmail.com>
2026-06-16 12:01:21 -0400

    File cleanup

--- eoupv2/__init__.py
+++
@@ -1,8 +0,0 @@
-"""EOUP v2 - Earth Observations Using Python"""
-
-__version__ = "1.0.0"
-
-from .data import setup_book_data, load_chapter_data
-from .utils import *
-
-__all__ = ["setup_book_data", "load_chapter_data"]
